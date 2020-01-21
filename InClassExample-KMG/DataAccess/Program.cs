1c1309f322aed02d86fe0a813465ae0a4daab2d9
Kala Rodriguez <kala.rodriguez@ou.edu>
2020-01-21 12:46:08 -0600

    called Add and Subtract method and displayed results

--- InClassExample-KMG/DataAccess/Program.cs
+++ InClassExample-KMG/DataAccess/Program.cs
@@ -12,7 +12,19 @@ namespace DataAccess
     {
         static void Main(string[] args)
         {
+            double num1, num2, num3, num4;
+            num1 = 32.675;
+            num2 = 50.001;
+            num3 = 10.005;
+            num4 = 2.10;
 
+            double addResult = Add(num1, num2);
+            double subResult = Subtract(num3, num4);
+
+            Console.WriteLine($"{num1.ToString("N2")} + {num2.ToString("N2")} = {addResult.ToString("N2")}");
+            Console.WriteLine($"{num3.ToString("N2")} - {num4.ToString("N2")} = {subResult.ToString("N2")}");
+
+            Console.ReadKey();
 
         }
 
@@ -21,6 +33,8 @@ namespace DataAccess
             var sum = value1 + value2;
             return sum;
         }
+
+
 
         static double Subtract(double value1, double value2)
         {            
