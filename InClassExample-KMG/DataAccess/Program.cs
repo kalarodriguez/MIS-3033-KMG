fa7cc4dd87a1f4b26b68abd2fc603c6a54d787bc
Kala Rodriguez <kala.rodriguez@ou.edu>
2020-01-21 12:30:26 -0600

    created project & solution - data access.

---
+++ InClassExample-KMG/DataAccess/Program.cs
@@ -0,0 +1,17 @@
+﻿//Kala Rodriguez
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
