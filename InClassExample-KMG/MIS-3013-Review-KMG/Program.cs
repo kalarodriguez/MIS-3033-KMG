3df1ff53001a65bfcd3d2e285c2e81f700933aa5
Kala Rodriguez <kala.rodriguez@ou.edu>
2020-01-23 12:25:21 -0600

    parallel array example

--- InClassExample-KMG/MIS-3013-Review-KMG/Program.cs
+++ InClassExample-KMG/MIS-3013-Review-KMG/Program.cs
@@ -10,6 +10,27 @@ namespace MIS_3013_Review_KMG
     {
         static void Main(string[] args)
         {
+            // Parallel Arrays = means each element at the same position is part of the same entity
+            int[] studentIDs = new int[3];
+            string[] studentFirstNames = new string[3];
+            string[] studentLastNames = { "Guthrie", "Mallory", "Johnson" };
+            studentIDs[0] = 1;
+            studentIDs[1] = 2;
+            studentIDs[2] = 3;
+            studentFirstNames[0] = "Jordan";
+            studentFirstNames[1] = "Harvey";
+            studentFirstNames[2] = "William";
+
+            // any time you have a collection you will have to use a loop to iterate each item in the collection
+
+            for (int i = 0;  i < studentIDs.Length; i++)
+            {
+                Console.WriteLine($"{studentLastNames[i]}, {studentFirstNames[i]} ({studentIDs[i]})");
+            }
+
+
+            Console.ReadKey();
+
         }
     }
 }
